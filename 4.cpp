226c6dbe95c36e30dc26d5ebc727572ea8dd7351
LiHzz <LiHzz0323@gmail.com>
2017-08-22 14:52:34 +0800

    Initial commit

---
+++ 4.cpp
@@ -0,0 +1,26 @@
+#include<algorithm>
+#include<vector>
+//
+// Created by lh on 2017/8/22.
+//
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> v3 = merge(nums1,nums2);
+        sort(v3.begin(),v3.end());
+        int len = v3.size();
+        if (len % 2 == 0){
+            int med = len /2;
+            return (v3[med] + v3[med-1])/2.0;
+        } else{
+            int med = len/2;
+            return v3[med];
+        }
+    }
+
+    vector<int> merge(vector<int> v1,vector<int> v2){
+        v1.insert(v1.end(),v2.begin(),v2.end());
+        return  v1;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "4.cpp"
+using namespace std;
+int main() {
+    Solution sl;
+    vector<int> v1,v2;
+    v1.push_back(1);
+    v1.push_back(2);
+    v2.push_back(3);
+    v2.push_back(4);
+    cout << sl.findMedianSortedArrays(v1,v2);
+    return 0;
+}
