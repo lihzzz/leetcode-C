bf5593782bc647da645f8d87cbfb3189de5ee042
LiHzz <LiHzz0323@gmail.com>
2017-09-07 17:15:45 +0800

    update

--- code/300.cpp
+++ code/300.cpp
@@ -3,26 +3,62 @@
 //
 
 #include <vector>
-
+#include <iostream>
 using namespace std;
+
+//class Solution{
+//public:
+//    int lengthOfLIS(vector<int>& nums){
+//        int _max = 0;
+//        if(nums.size() == 0)
+//            return 0;
+//        vector<int> dp(nums.size(),1);
+//        for (int i = 0; i <nums.size() ; ++i) {
+//            for (int j = 0; j <i ; ++j) {
+//                if(nums[j] < nums[i]){
+//                    dp[i] = max(dp[j]+1,dp[i]);
+//                }
+//            }
+//            if(dp[i] > _max){
+//                _max = dp[i];
+//            }
+//        }
+//        return _max;
+//    }
+//};
 
 class Solution{
 public:
-    int lengthOfLIS(vector<int>& nums){
-        int _max = 0;
-        if(nums.size() == 0)
-            return 0;
-        vector<int> dp(nums.size(),1);
-        for (int i = 0; i <nums.size() ; ++i) {
-            for (int j = 0; j <i ; ++j) {
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[j]+1,dp[i]);
-                }
-            }
-            if(dp[i] > _max){
-                _max = dp[i];
+    int CeilIndex(vector<int>& nums,int l,int r,int key){
+        while (l<=r){
+            int m = (l+r)/2;
+            if(nums[m] >= key){
+                r = m-1;
+            }else{
+                l = m + 1;
             }
         }
-        return _max;
+        return l;
+    }
+    int lengthOfLIS(vector<int>& nums){
+        if(nums.size() == 0)
+            return 0;
+        vector<int> tail(nums.size(),0);
+        int length = 1;
+        tail[0] = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] < tail[0])
+                tail[0] = nums[i];
+            else if(nums[i] > tail[length-1])
+                tail[length++] = nums[i];
+            else
+                tail[CeilIndex(tail,0,length-1,nums[i])] = nums[i];
+        }
+        for (int j = 0; j <nums.size() ; ++j) {
+            cout << tail[j] << " ";
+        }
+
+        return length;
     }
 };
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main() {
     Solution sl;
-    int coin[] = {1,3,6,7,9,4,10,5,6};
+    int coin[] = {2, 5, 3, 7, 11, 8, 10, 13, 6};
     vector<int> coins(begin(coin),end(coin));
     cout << sl.lengthOfLIS(coins);
     return 0;
