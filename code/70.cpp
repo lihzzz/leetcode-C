4564a86594f04c3e203e3c246758d8ed992a2738
LiHzz <LiHzz0323@gmail.com>
2017-09-08 13:00:58 +0800

    update

--- code/70.cpp
+++ code/70.cpp
@@ -1,16 +1,42 @@
 //
 // Created by lh on 2017/9/8.
 //
+#include <vector>
+using namespace std;
+//class Solution{
+//public:
+//    int climbStairs(int n){
+//        int p = 1,q = 1;
+//        for (int i = 2; i <=n ; ++i) {
+//            int temp = q;
+//            q += p;
+//            p = temp;
+//        }
+//        return q;
+//    }
+//};
+
+//class Solution{
+//public:
+//    int climbStairs(int n){
+//        if(n<=1) return 1;
+//        vector<int> dp(n,0);
+//        dp[0]=1;dp[1] = 2;
+//        for (int i = 2; i <n ; ++i) {
+//            dp[i] = dp[i-1] + dp[i-2];
+//        }
+//        return dp.back();
+//    }
+//};
 
 class Solution{
 public:
-    int climbstairs(int n){
-        int p = 1,q = 1;
-        for (int i = 2; i <=n ; ++i) {
-            int temp = q;
-            q += p;
-            p = temp;
+    int climbStairs(int n){
+        int a = 1,b = 1;
+        while (n--){
+            b += a;
+            a = b-a;
         }
-        return q;
+        return a;
     }
 };
