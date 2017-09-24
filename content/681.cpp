ecc9a9d05fe76e61ed4b2a94ca780b78495f5894
LiHzz <LiHzz0323@gmail.com>
2017-09-24 22:22:38 +0800

    update

--- content/681.cpp
+++ content/681.cpp
@@ -5,15 +5,25 @@
 #include <vector>
 using std::vector;
 using std::string;
+using std::stoi;
+using std::to_string;
 class Solution {
 public:
     string nextClosestTime(string time) {
-        string res ="";
-        vector<int> m;
-        for (int i = 0; i <time.size() ; ++i) {
-            if(time[i] == ':')continue;
-            m.push_back(int(time[i]));
+        string res = "0000";
+        int index = time.find(':');
+        int cur = stoi(time.substr(0,index)) * 60 + stoi(time.substr(index+1));
+        for (int i = 1, d = 0; i <=1440 && d<4 ; ++i) {
+            int m = (cur + i) % 1440;
+            for (d = 0; d <4 ; ++d) {
+                res[d] = '0' + m / mins[d];
+                m %= mins[d];
+                if(time.find(res[d]) == string::npos) break;
+            }
         }
-
+        return res.substr(0,2) + ":" + res.substr(2);
     }
+
+private:
+    int mins[4] = {600,60,10,1};
 };
--- content/683.cpp
+++ content/683.cpp
@@ -3,14 +3,27 @@
 //
 
 #include <vector>
+#include <set>
+using std::set;
 using std::vector;
 
 class Solution {
 public:
     int kEmptySlots(vector<int>& flowers, int k) {
-        int left = 0,right  = 0;
+        set<int> m;
         for (int i = 0; i <flowers.size() ; ++i) {
+            int cur = flowers[i];
+            m.insert(cur);
+            auto it = m.find(cur);
+            if(it != m.begin() ){
+                if(cur - *(--it) == k+1)
+                    return i+1;
+                it ++;
+            }
 
+            if(it!= m.end() && *(++it) - cur == k+1)
+                return i+1;
         }
+        return -1;
     }
 };
