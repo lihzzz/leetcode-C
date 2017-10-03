7412610b4935c5e67a34d6f2cb2a166a14a5541a
LiHzz <LiHzz0323@gmail.com>
2017-10-03 23:58:18 +0800

    update

--- medium/540.cpp
+++ medium/540.cpp
@@ -6,21 +6,35 @@
 
 using std::unique;
 using std::vector;
+//class Solution {
+//public:
+//    int singleNonDuplicate(vector<int>& nums) {
+//        int res = 0;
+//        for (int i = 0; i <nums.size() ;) {
+//            if(nums[i] != nums[i+1] || i+1 == nums.size()){
+//                res = nums[i];
+//                break;
+//            }
+//            i += 2;
+//        }
+//        return res;
+//    }
+//};
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int res = 0;
-        if(!nums.empty()){
-            int n = nums.size();
-            vector<int>::iterator it =  unique(nums.begin(),nums.end());
-            for (int i = 0; i <(n+1)/2 ; ++i) {
-                if(nums[i]!=*it){
-                    res = nums[i];
-                    break;
-                }
-                ++it;
+        int left = 0 ,right = nums.size()-1;
+        while (left<right){
+            int mid = left + (right-left)/2;
+            int even = (mid%2==0)?nums[mid]:nums[mid-1];
+            int nextnum = (mid%2 == 0)?nums[mid+1]:nums[mid];
+
+            if(even == nextnum){
+                left = mid +1;
+            } else{
+                right = (mid/2)*2;
             }
         }
-        return res;
+        return nums[left];
     }
 };
