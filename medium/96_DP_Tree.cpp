e92caf6642b4395bdedf55b9bf3a29f73946a0a0
LiHzz <LiHzz0323@gmail.com>
2017-10-14 23:30:22 +0800

    update

---
+++ medium/96_DP_Tree.cpp
@@ -0,0 +1,22 @@
+//
+// Created by lh on 2017/10/14.
+//
+
+#include "../DataStruct.h"
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <=n ; ++i) {
+            for (int j = 0; j <i ; ++j) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+};
