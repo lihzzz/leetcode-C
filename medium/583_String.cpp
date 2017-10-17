07161d1eca047c3d925c8f27d6cca37539c20cd1
LiHzz <LiHzz0323@gmail.com>
2017-10-17 15:16:33 +0800

    update

--- medium/583_String.cpp
+++ medium/583_String.cpp
@@ -2,10 +2,22 @@
 // Created by lh on 2017/10/16.
 //
 #include <string>
+#include <vector>
 using namespace std;
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-
+        int n = word1.size(),m = word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for (int i = 1; i <=n ; ++i) {
+            for (int j = 1; j <=m ; ++j) {
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n+m-2*dp[n][m];
     }
 };
