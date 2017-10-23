4d9c6e9af2f8dff8f3d84b12dc34b14978830e9c
LiHzz <LiHzz0323@gmail.com>
2017-10-23 22:17:07 +0800

    update

--- content/17-10-22/714.cpp
+++ content/17-10-22/714.cpp
@@ -8,28 +8,17 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int buyVal = prices[0];
-        int sellVal = prices[0]; int profit = 0;
-        for (int i = 1; i < prices.size(); i++)
-        {
-            if (prices[i] >= prices[i - 1])
-            {
-                sellVal = prices[i];
-            }
-            else
-            {
-                if (sellVal - buyVal > fee)
-                {
-                    profit += sellVal - buyVal - fee;
-                }
-                buyVal = prices[i];
-                sellVal = prices[i];
-            }
+        int n = prices.size();
+        if(n<=1)
+            return 0;
+        vector<int> buy(n,0);
+        vector<int> sell(n,0);
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        for (int i = 1; i <n ; ++i) {
+            buy[i] = max(buy[i-1],sell[i-1]-prices[i]);
+            sell[i] = max(sell[i-1],buy[i-1]+prices[i]-fee);
         }
-        if (sellVal - buyVal > fee)
-        {
-            profit += sellVal - buyVal - fee;
-        }
-        return profit;
+        return sell[n-1];
     }
 };
--- main.cpp
+++ main.cpp
@@ -31,24 +31,16 @@ int main() {
     vector<string> strs = {"5","2","C","D","+"};
     string s = "leeeeetcode";
     string t = "ss";
-    unordered_map<int,int>m;
-    for (int i = 10; i <20 ; ++i) {
-        m[i]=1;
-    }
-    for (int j = 30; j <50 ; ++j) {
-        m[j]=1;
-    }
-    for(int i = 0;i<50;i++){
-        cout<< m[i]<<endl;
-    }
-    RangeModule rm;
-    rm.addRange(10,180);
-    rm.addRange(150,200);
-    rm.addRange(250,500);
-    cout<< rm.queryRange(50,100);
-    cout << rm.queryRange(180,300);
-    cout << rm.queryRange(600,1000);
-    rm.removeRange(50,100);
-    cout << rm.queryRange(50,100);
+    cout << sizeof(int) << endl;
+
+//    RangeModule rm;
+//    rm.addRange(10,180);
+//    rm.addRange(150,200);
+//    rm.addRange(250,500);
+//    cout<< rm.queryRange(50,100);
+//    cout << rm.queryRange(180,300);
+//    cout << rm.queryRange(600,1000);
+//    rm.removeRange(50,100);
+//    cout << rm.queryRange(50,100);
     return 0;
 }
