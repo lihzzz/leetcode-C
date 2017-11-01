47a825bb9a4cdfd8873fe62701dabf077e42500f
JOTime <LiHzz0323@gmail.com>
2017-11-01 22:11:38 +0800

    modify

---
+++ medium/173_Stack_Tree_Design.cpp
@@ -0,0 +1,21 @@
+//
+// Created by lll on 2017/11/1.
+//
+#include "../DataStruct.h"
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+
+    }
+
+    /** @return the next smallest number */
+    int next() {
+
+    }
+};
