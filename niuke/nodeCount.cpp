1149fc27f521a7acc5f1112d4ad61680c7ef7378
LiHzz <LiHzz0323@gmail.com>
2017-11-27 12:54:48 +0800

    update

--- niuke/nodeCount.cpp
+++ niuke/nodeCount.cpp
@@ -1,3 +1,27 @@
 //
 // Created by lh on 2017/11/21.
 //
+
+
+//#include <cmath>
+//#include <iostream>
+//using namespace std;
+//int countNode(int n, int m){
+//    int res;
+//    int k = int(log((n*1.0/m)) / log(2.0))+1;
+//    int tmp = n - pow(2,k-1)*m;
+//    if(tmp >= pow(2,k-1)){
+//        res = pow(2,k)-1;
+//    }else{
+//        res = pow(2,k-1)+tmp;
+//    }
+//    return res;
+//}
+//int main(){
+//    int m,n;
+//    while(cin >> m>>n){
+//        if(m==0 && n==0) break;
+//        cout <<countNode(n,m)<<endl;
+//    }
+//    return 0;
+//}
---
+++ niuke/stringMatching.cpp
@@ -0,0 +1,32 @@
+//
+// Created by lh on 2017/11/27.
+//
+#include <string>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+bool IsAllCharExist(string str1,string str2){
+    unordered_map<char,int> m;
+    int n = str1.size(),l = str2.size();
+    if(l == 0)
+        return false;
+    for (int i = 0; i <l ; ++i) {
+        m[str2[i]]++;
+    }
+    for (int j = 0; j <n ; ++j) {
+        if(m[str1[j]] == 0)
+            return false;
+    }
+    return true;
+}
+int main(){
+    string str1,str2;
+    while (cin>>str1>>str2){
+        if(IsAllCharExist(str1,str2))
+            cout<< "true" << endl;
+        else
+            cout<< "false" << endl;
+    }
+    return 0;
+}
