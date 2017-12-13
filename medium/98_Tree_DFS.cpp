dae5dca6717258c912c670d814de2f9bfb887081
LiHzz <LiHzz0323@gmail.com>
2017-12-13 18:04:28 +0800

    update

--- medium/98_Tree_DFS.cpp
+++ medium/98_Tree_DFS.cpp
@@ -2,15 +2,26 @@
 // Created by LH on 2017/12/12.
 //
 #include "../DataStruct.h"
+
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if(!root || (isValidBST(root->left) && isValidBST(root->right)))
+        if (!root)
             return true;
-        if(root->left){
-
+        else if(isValidBST(root->left) && isValidBST(root->right) &&
+                (!root->left || root->left && isNodeValid(root->val,root->left,0) ) &&
+                (!root->right || root->right && isNodeValid(root->val,root->right,1)  ))
+            return true;
+        else
+            return false;
+    }
+    bool isNodeValid(int num,TreeNode* t,int flag){
+        if(!t)
+            return true;
+        if(flag){
+            return (num<t->val && isNodeValid(num,t->left,flag) && isNodeValid(num,t->right,flag));
+        }else{
+            return (num > t->val && isNodeValid(num,t->left,flag) && isNodeValid(num,t->right,flag));
         }
     }
-
-
 };
